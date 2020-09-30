f67dfff51244065789f993deaf8ca690f4364e56
minheekangg <minheekangg@gmail.com>
2020-09-30 11:13:16 -0400

    created search that handles input change and submit

---
+++ movie-search/src/Components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Search from './Search';
+
+
+console.log(process.env)
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Movie Search App
+      </header>
+      <Search />
+    </div>
+  );
+}
+
+export default App;
---
+++ movie-search/src/Components/Search.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default class Search extends React.Component {
+    state = {
+        search: '',
+    }
+
+    handleSubmit (e) {
+        e.preventDefault();
+
+        console.log(this.state.search);
+
+    }
+
+    render() {
+        return (
+            <form onSubmit={e=>this.handleSubmit(e)}>
+                <input value={this.state.search} onChange={e=>this.setState({search: e.target.value})}/>
+                <button />
+            </form>
+        )
+    }
+
+}
