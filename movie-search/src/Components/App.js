acb1fe9f0bbbf7f9d726e17fb09b32249ef2e1c7
minheekangg <minheekangg@gmail.com>
2020-09-30 13:08:02 -0400

    separated fetch fn so two actions can fetch

--- movie-search/src/Components/App.js
+++ movie-search/src/Components/App.js
@@ -8,29 +8,43 @@ import './App.css';
 
 class App extends React.Component {
   state = {
-    movies: []
+    movies: [],
+    page: 1,
+    search: '',
   };
 
-  handleSearch = searchQuery => {
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${searchQuery}`)
-      .then(res=> res.json())
+  handleFetch() {
+    const { search, page } = this.state;
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${search}&page=${page}`)
+      .then(res => res.json())
       .then(result => {
-        const {results} = result;
+        const { results } = result;
+        console.log(result)
         this.setState({ movies: results }, () => {
           console.log('after fetch state has been changed to :', this.state);
         });
       })
-      .catch(err=>console.log('err', err))
+      .catch(err => console.log('err', err))
+  }
+
+  handleSearch = searchQuery => {
+    if (searchQuery === this.state.search) return;
+    this.setState({ search: searchQuery }, () => this.handleFetch());
+  }
+
+  handleLoadMore = () => {
+    this.setState({ page: this.state.page + 1 }, () => this.handleFetch());
   }
 
   render() {
+    const { movies } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <h3>Movie Search App</h3>
         </header>
         <Search handleSearch={this.handleSearch}/>
-        <MovieContainer movies={this.state.movies}/>
+        <MovieContainer movies={movies} handleLoadMore={this.handleLoadMore}/>
       </div>
     );
   }
--- movie-search/src/Components/Movie-Card.js
+++ movie-search/src/Components/Movie-Card.js
@@ -5,13 +5,15 @@ function MovieCard(props) {
     const { title, vote_average, vote_count, release_date, overview, poster_path } = props.movie;
 
     return (
-        <div class="movie-container">
-            <img class="movie-container-left" src={`https://image.tmdb.org/t/p/w300${poster_path}`} alt={title}/>
-            <div class="movie-container-right">
+        <div className="movie-container">
+            <div className="movie-container-left">
+                {poster_path && <img src={`https://image.tmdb.org/t/p/w300${poster_path}`} alt={title} />}
+            </div>
+            <div className="movie-container-right">
                 <h6>{title}</h6>
-                <p class="movie-metadata">{trimYear(release_date)}</p>
-                <p class="movie-description">{overview}</p>
-                <div class="movie-metadata"> 
+                <p className="movie-metadata">{trimYear(release_date)}</p>
+                <p className="movie-description">{overview}</p>
+                <div className="movie-metadata"> 
                     <p>{vote_average}</p> 
                     <p>{vote_count}</p> 
                 </div>
