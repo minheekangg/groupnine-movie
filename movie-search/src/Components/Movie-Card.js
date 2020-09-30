a892d66e0539a36c6b8c8b6395f94c9b5494ab3f
minheekangg <minheekangg@gmail.com>
2020-09-30 12:44:05 -0400

    added img alt and trim title

--- movie-search/src/Components/Movie-Card.js
+++ movie-search/src/Components/Movie-Card.js
@@ -6,10 +6,10 @@ function MovieCard(props) {
 
     return (
         <div class="movie-container">
-            <img class="movie-container-left" src={`https://image.tmdb.org/t/p/w154${poster_path}`}/>
+            <img class="movie-container-left" src={`https://image.tmdb.org/t/p/w300${poster_path}`} alt={title}/>
             <div class="movie-container-right">
                 <h6>{title}</h6>
-                <p class="movie-metadata">{release_date}</p>
+                <p class="movie-metadata">{trimYear(release_date)}</p>
                 <p class="movie-description">{overview}</p>
                 <div class="movie-metadata"> 
                     <p>{vote_average}</p> 
@@ -21,3 +21,11 @@ function MovieCard(props) {
 }
 
 export default MovieCard;
+
+
+function trimYear (date) {
+    if (!date || !date.length) return;
+
+    const splitDate = date.split("-");
+    return splitDate[0];
+}
