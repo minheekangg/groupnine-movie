495220708a1bc14c09cc2993b192ee11ac1620d5
minheekangg <minheekangg@gmail.com>
2020-09-30 11:18:32 -0400

    added initial styling

--- movie-search/src/Components/App.js
+++ movie-search/src/Components/App.js
@@ -3,13 +3,15 @@ import React from 'react';
 import Search from './Search';
 import MovieContainer from './Movie-Container';
 
+import './App.css';
+
 console.log(process.env)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Movie Search App
+        <h3>Movie Search App</h3>
       </header>
       <Search />
       <MovieContainer />
