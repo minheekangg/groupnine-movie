3ca68b9c03f85459003e6cd5c8f72fe28e3e823e
minheekangg <minheekangg@gmail.com>
2020-09-30 11:41:21 -0400

    handle search fetches query successfully

--- movie-search/src/Components/App.js
+++ movie-search/src/Components/App.js
@@ -5,16 +5,24 @@ import MovieContainer from './Movie-Container';
 
 import './App.css';
 
-console.log(process.env)
 
 class App extends React.Component {
   state = {
     movies: [],
+    search: 'd'
   };
 
-  handleSearch(e) {
+  handleSearch = e => {
     console.log('inside app search is', e);
-    
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${this.state.search}`)
+      .then(res=> res.json())
+      .then(result => {
+        const {results} = result;
+        this.setState({ movies: results }, () => {
+          console.log('after fetch state has been changed to :', this.state);
+        });
+      })
+      .catch(err=>console.log('err', err))
   }
 
   render() {
--- movie-search/src/Components/Search.js
+++ movie-search/src/Components/Search.js
@@ -8,6 +8,7 @@ export default class Search extends React.Component {
     handleSubmit (e) {
         e.preventDefault();
         //TODO: validation!
+        //TODO: changes need to be triggered by SEARCH! not submit!
         this.props.handleSearch(this.state.search);
     }
 
